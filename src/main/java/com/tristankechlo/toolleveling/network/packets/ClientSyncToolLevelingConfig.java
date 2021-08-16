67fd31cbad13a04063503a0fc7122d33f9008a0d
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2021-08-16 17:05:01 +0200

    Update ClientSyncToolLevelingConfig.java

--- src/main/java/com/tristankechlo/toolleveling/network/packets/ClientSyncToolLevelingConfig.java
+++ src/main/java/com/tristankechlo/toolleveling/network/packets/ClientSyncToolLevelingConfig.java
@@ -7,18 +7,22 @@ import org.apache.logging.log4j.Level;
 import com.tristankechlo.toolleveling.ToolLeveling;
 import com.tristankechlo.toolleveling.config.ConfigManager;
 
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fmllegacy.network.NetworkEvent;
 
 public class ClientSyncToolLevelingConfig {
 
 	public static void handle(SyncToolLevelingConfig msg, Supplier<NetworkEvent.Context> context) {
-		boolean check = ConfigManager.deserializeConfig(msg.identifier, msg.json);
-		if (!check) {
-			ToolLeveling.LOGGER.log(Level.ERROR, "Config " + msg.identifier + " could not be loaded");
-			throw new RuntimeException("Config " + msg.identifier + " could not be loaded");
-		} else {
-			ToolLeveling.LOGGER.log(Level.INFO, "Config " + msg.identifier + " recieved and loaded.");
-		}
+		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
+			boolean check = ConfigManager.deserializeConfig(msg.identifier, msg.json);
+			if (!check) {
+				ToolLeveling.LOGGER.log(Level.ERROR, "Config " + msg.identifier + " could not be loaded");
+				throw new RuntimeException("Config " + msg.identifier + " could not be loaded");
+			} else {
+				ToolLeveling.LOGGER.log(Level.INFO, "Config " + msg.identifier + " recieved and loaded.");
+			}
+		});
 	}
 
 }
