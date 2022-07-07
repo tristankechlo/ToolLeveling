83a563175c38bf7d26abdd15b9b2fd5042db9951
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2022-07-08 00:20:36 +0200

    move the command to view the item values from `/openitemvalues` to `/toolleveling openitemvalues`

--- src/main/java/com/tristankechlo/toolleveling/commands/ToolLevelingCommand.java
+++ src/main/java/com/tristankechlo/toolleveling/commands/ToolLevelingCommand.java
@@ -3,7 +3,10 @@ package com.tristankechlo.toolleveling.commands;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.tristankechlo.toolleveling.config.ConfigManager;
+import com.tristankechlo.toolleveling.network.PacketHandler;
+import com.tristankechlo.toolleveling.network.packets.OpenItemValueScreenPacket;
 import com.tristankechlo.toolleveling.utils.Names;
 
 import net.minecraft.ChatFormatting;
@@ -12,22 +15,26 @@ import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.network.PacketDistributor;
 import net.minecraftforge.server.command.EnumArgument;
 
 public final class ToolLevelingCommand {
 
 	public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
-		LiteralArgumentBuilder<CommandSourceStack> toollevelingCommand = Commands.literal("toolleveling")
-				.requires(
-						(source) -> source.hasPermission(3))
-				.then(Commands.literal("config")
+		LiteralArgumentBuilder<CommandSourceStack> toollevelingCommand = Commands
+				.literal(
+						"toolleveling")
+				.then(Commands.literal("config").requires((source) -> source.hasPermission(3))
 						.then(Commands.literal("reload").executes(context -> configReload(context)))
 						.then(Commands.literal("show")
 								.then(Commands.argument("identifier", EnumArgument.enumArgument(Identifier.class))
 										.executes(context -> configShow(context))))
 						.then(Commands.literal("reset")
 								.then(Commands.argument("identifier", EnumArgument.enumArgument(Identifier.class))
-										.executes(context -> configReset(context)))));
+										.executes(context -> configReset(context)))))
+				.then(Commands.literal("openitemvalues").requires((source) -> source.hasPermission(0))
+						.executes((c) -> showScreen(c)));
 		dispatcher.register(toollevelingCommand);
 	}
 
@@ -67,6 +74,12 @@ public final class ToolLevelingCommand {
 		return 1;
 	}
 
+	private static int showScreen(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
+		ServerPlayer player = context.getSource().getPlayerOrException();
+		PacketHandler.INSTANCE.send(PacketDistributor.PLAYER.with(() -> player), new OpenItemValueScreenPacket());
+		return 1;
+	}
+
 	private static enum Identifier {
 		GENERAL(Names.MOD_ID + ":general"),
 		ITEMVALUES(Names.MOD_ID + ":itemValues");
@@ -77,4 +90,5 @@ public final class ToolLevelingCommand {
 			this.id = id;
 		}
 	}
+
 }
