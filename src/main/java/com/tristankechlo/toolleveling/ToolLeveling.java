ec374bf3de53b50eaf9be4de9395a645a790c19b
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2022-07-10 21:20:08 +0200

    rename config-identifier from `itemValues` to `item_values`

--- src/main/java/com/tristankechlo/toolleveling/ToolLeveling.java
+++ src/main/java/com/tristankechlo/toolleveling/ToolLeveling.java
@@ -1,14 +1,11 @@
 package com.tristankechlo.toolleveling;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
+import com.mojang.logging.LogUtils;
 import com.tristankechlo.toolleveling.client.ClientSetup;
 import com.tristankechlo.toolleveling.config.ConfigManager;
 import com.tristankechlo.toolleveling.init.ModRegistry;
 import com.tristankechlo.toolleveling.network.PacketHandler;
 import com.tristankechlo.toolleveling.utils.Names;
-
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent;
@@ -17,11 +14,12 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
 
 @Mod(Names.MOD_ID)
 public final class ToolLeveling {
 
-	public static final Logger LOGGER = LogManager.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
 	public ToolLeveling() {
 		final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
